69de7a5d4b82cce975e6fadef26568155c0e072d
Lynx <dotchucknorris@gmx.de>
2021-07-24 16:08:19 +0200

    Client: Implement ship movement

--- client/Scenes/Entities/Player.gd
+++ client/Scenes/Entities/Player.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
+export var move_speed = 500
+
 func _process(delta):
-	#translate(Vector2(0, -100 * delta))
 	look_at(get_global_mouse_position())
+	
+	if Input.is_action_pressed("boost"):
+		position += (Vector2.RIGHT * move_speed).rotated(rotation) * delta
+
+func _physics_process(delta):
 	pass
