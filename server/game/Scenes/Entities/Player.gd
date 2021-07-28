587405ef5f84640c4e0f47e399b0039561dc7f68
Lynx <dotchucknorris@gmx.de>
2021-07-29 01:25:32 +0200

    GameServer: Implement player life regen

--- server/game/Scenes/Entities/Player.gd
+++ server/game/Scenes/Entities/Player.gd
@@ -2,12 +2,26 @@ class_name Player
 extends Node2D
 
 export var max_health = 100
+export var regen_interval = 1
+export var regen_amount = 5
 
 var peer_id = 0
 
 var health = 100
+var regen_time = regen_interval
 
 var last_state_time = 0
+
+func _process(delta):
+	if health < max_health:
+		regen_time -= delta
+		if regen_time <= 0:
+			health += regen_amount
+			if health > max_health:
+				health = max_health
+				regen_time = regen_interval
+			else:
+				regen_time += regen_interval
 
 func create_state():
 	return {
@@ -31,3 +45,5 @@ func damage(peer_id, damage):
 	if peer_id == self.peer_id:
 		return # self hurting is evil!
 	health -= damage
+	if health < 0:
+		health = 0 # dead is dead 
