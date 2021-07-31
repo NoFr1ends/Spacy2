8f1c7da49305cd815db183116498a860af7569fd
Lynx <dotchucknorris@gmx.de>
2021-07-31 18:41:00 +0200

    GameServer: Implement damage when player is outside play area

--- server/game/Scenes/Entities/Player.gd
+++ server/game/Scenes/Entities/Player.gd
@@ -1,6 +1,8 @@
 class_name Player
 extends Node2D
 
+onready var gs = $"../../"
+
 export var max_health = 100
 export var regen_interval = 1
 export var regen_amount = 5
@@ -9,10 +11,23 @@ var peer_id = 0
 
 var health = 100
 var regen_time = regen_interval
+var play_area_damage_time = 1
 
 var last_state_time = 0
 
 func _process(delta):
+	var distance = position.distance_to(Vector2.ZERO)
+	if distance > gs.play_area_size:
+		var over = distance - gs.play_area_size
+		play_area_damage_time -= delta
+		regen_time = regen_interval
+		if play_area_damage_time <= 0:
+			health -= int(over / 50)
+			play_area_damage_time += 1
+	else:
+		play_area_damage_time = 1
+		
+	
 	if health < max_health:
 		regen_time -= delta
 		if regen_time <= 0:
